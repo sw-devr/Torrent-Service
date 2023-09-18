a0846d49f27891a670aa4f346e4cd7acefc6f53a
kyo705 <mission0705@naver.com>
2023-09-18 10:28:56 +0900

    [ADD] ClientSocketHandler

---
+++ src/main/client/ClientSocketHandler.java
@@ -0,0 +1,17 @@
+package main.client;
+
+import main.protocol.SocketRequest;
+import main.protocol.SocketResponse;
+
+public class ClientSocketHandler {
+
+
+    public void sendRequest(SocketRequest request) {
+
+    }
+
+    public SocketResponse receiveResponse() {
+
+        return null;
+    }
+}
